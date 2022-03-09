dda6a0aafda35809897af87d344095480bc95859
Don Peat <don@caleos.io>
2022-03-09 13:30:52 -0600

    fix: remove analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,6 @@ const App = ({ Component, pageProps }: AppProps) => (
     />
     <Component {...pageProps} />
     <Script>{`window.sa_event=window.sa_event||function(){var a=[].slice.call(arguments);window.sa_event.q?window.sa_event.q.push(a):window.sa_event.q=[a]};`}</Script>
-    <Script async defer src="https://scripts.simpleanalyticscdn.com/latest.js" />
   </>
 );
 
