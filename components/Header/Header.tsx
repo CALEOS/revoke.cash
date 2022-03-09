b72d3444724b70a74ffc52d90505bda26a28dbb9
Don Peat <don@caleos.io>
2022-03-09 13:06:52 -0600

    feat: disclaimer

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,6 +6,9 @@ import ConnectButton from './ConnectButton';
 const Header: React.FC = () => {
   return (
     <Row className="Header">
+      {"\n"}
+      <div className='disclaimer'> DISCLAIMER: some allowances related to adding liquidity are not being removed as expected. Until we can address this issue, check that permissions have been removed after using this tool. If needed, manually disconnect from applications via 'connected sites' in MetaMask to revoke all permissions.</div>
+      {"\n"}
       <Col className="my-auto">
         <div className="only-mobile" style={{ float: 'left' }}><DonateButton /></div>
       </Col>
